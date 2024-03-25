15f4cc3723a41810bc4eb3d4bffdaf68ae77449f
Vamsi Gajula <gajulavamsi90@gmail.com>
2024-03-25 17:51:47 +0530

    fsd files

---
+++ app.js
@@ -0,0 +1,8 @@
+import Counter from "./components/Counter";
+function App() {
+return (
+<div>
+<Counter/>
+</div>
+); 
+}
---
+++ counter.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+class Counter extends Component{
+constructor(props){
+super(props)
+this.state={count:0}
+}
+render(){
+return(<button onClick={()=>this.setState
+({count:this.state.count+1})}>count={this.state.count}
+</button>)
+}
+}
+export default Counter
